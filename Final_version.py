1595bd8f0520d3927199a37ef61b31d1ce0d1cac
Carlos Escarcena <c.escarcena.2021@alumnos.urjc.es>
2023-10-13 16:42:53 +0200

    Follow line update

--- Final_version.py
+++ Final_version.py
@@ -33,8 +33,7 @@ def spiral(radius):
 
 def turn():
     HAL.setV(0.5)
-    HAL.setW(2.5)
-    time.sleep(1)
+    HAL.setW(-2.5)
     return 0
 
 
@@ -42,22 +41,34 @@ state = 0
 # state 0 = spiral
 # state 1 = go_backwards
 # state 2 = forwards
+# state 3 = turn 
 
 state_start_time = time.time()
+go_backwards_time = 5
 state_duration = random.uniform(0,4)
-angle = 3
+angle = 2
 forward_speed = 0
 time_max = random.uniform(2,5)
+counter = 0
 
 while True:
+
+    forward_duration = random.uniform(4,8)
     laser_data = HAL.getLaserData()
     laser = parse_laser_data(laser_data)
     laser_dist = laser[90][0]
-    # print(laser_dist)
+    bumper = HAL.getBumperData().state
 
-    if laser_dist < 0.5:
+
+    if laser_dist < 0.35:
         state = 1
+        forward_speed = 0
 
+    if bumper == 1:
+        state = 3
+        forward_speed = 0
+
+    # SWITCH CASE
     if state == 0:
         
         HAL.setV(forward_speed)
@@ -65,7 +76,6 @@ while True:
         
         forward_speed += 0.01
         
-        print(forward_speed)
         if (forward_speed > time_max) :
           state = 2
           forward_speed = 0
@@ -73,11 +83,26 @@ while True:
 
     if state == 1:
         go_backwards()
+        
+        counter = counter + 0.01
+
         if time.time() - state_start_time >= 1:
-            state = 0
+            
+            state = 3
             state_start_time = time.time()  # Reinicia el temporizador
 
     if state == 2:
         forwards()
+        if time.time() - state_start_time >= forward_duration + counter:
+            counter = 0
+            state = 0
+            state_start_time = time.time()  # Reinicia el temporizador
+        if counter > 4:
+            counter = 0
+            
+    if state == 3:
+        turn()
+        if time.time() - state_start_time >= 0.5:
+            state = 2
+            state_start_time = time.time()  # Reinicia el temporizador
         
-    #print(state)
---
+++ v4.py
@@ -0,0 +1,124 @@
+from GUI import GUI
+from HAL import HAL
+import cv2 
+import numpy as np
+
+# Parámetros del controlador PID angular
+Kp_angular = 1
+Ki_angular = 0.0001
+Kd_angular = 2.5
+
+# Parámetros del controlador PID lineal
+Kp_lineal = 1
+Ki_lineal = 0.0001
+Kd_lineal = 2.5
+
+# Valores iniciales de las variables del controlador PID angular
+error_prev_angular = 0
+error_integral_angular = 0
+
+# Valores iniciales de las variables del controlador PID lineal
+error_prev_lineal = 0
+error_integral_lineal = 0
+
+# Función para calcular la señal de control PID
+def calculate_pid_angular(error):
+    global error_integral_angular
+    global error_prev_angular
+    
+    # Término proporcional
+    P = Kp_angular * error
+    
+    # Término integral
+    error_integral_angular += error
+    I = Ki_angular * error_integral_angular
+    
+    # Término derivativo
+    D = Kd_angular * (error - error_prev_angular)
+    
+    # Calcular la señal de control
+    control_signal = P + I + D
+    
+    # Actualizar el error previo
+    error_prev_angular = error
+    
+    return control_signal
+
+def calculate_pid_liner(error):
+    global error_integral_lineal
+    global error_prev_lineal
+    
+    # Calcula los términos PID inversos
+    P = Kp_lineal * error
+
+    error_integral_lineal += error
+    I = -Ki_lineal * error_integral_lineal
+    D = -Kd_lineal * (error - error_prev_lineal)
+
+    # Calcula la salida del controlador PID inverso
+    output = P + I + D
+
+    # Actualiza el error previo para la próxima iteración
+    error_prev_lineal = error
+
+    return output
+
+umbral_curba = 1
+
+while True:
+    # Enter iterative code!
+    v = 7
+    img = HAL.getImage()
+    height, width, channel = img.shape
+
+    # Calcular la altura de cada franja
+    altura_franja = height // 3
+
+    # Cortar la imagen en tres partes iguales
+    parte1 = img[0:altura_franja, :]
+    parte2 = img[altura_franja:2 * altura_franja, :]
+    parte3 = img[2 * altura_franja:3 * altura_franja, :]
+
+    # Guardar las partes en archivos separados
+    cv2.imwrite("parte1.jpg", parte1)
+    cv2.imwrite("parte2.jpg", parte2)
+    cv2.imwrite("parte3.jpg", parte3)
+
+    # image process to make the imgae more smooth so that we can obtain more information
+    hsv1 = cv2.cvtColor(parte2, cv2.COLOR_BGR2HSV)
+    blur1 = cv2.GaussianBlur(hsv1, (5,5), 0)
+
+    hsv2 = cv2.cvtColor(parte3, cv2.COLOR_BGR2HSV)
+    blur2 = cv2.GaussianBlur(hsv2, (5,5), 0)
+
+    # Filtrar el color rojo en la imagen
+    lower_red = (0, 43, 46)
+    upper_red = (26,255,255)
+
+    # detect the color of line
+    res1 = cv2.inRange(blur1, lower_red, upper_red)
+    res2 = cv2.inRange(blur2, lower_red, upper_red)
+    # d = cv.dilate(res, kernel=keneral, iterations=D_iter)
+    # e = cv.erode(d, kernel=keneral, iterations=E_iter)
+
+    # caculate the center of the line
+    m = cv2.moments(res1)
+    cx1 = int(m['m10']/m['m00'])
+    cy1 = int(m['m01']/m['m00'])
+
+    m = cv2.moments(res2)
+    cx2 = int(m['m10']/m['m00'])
+    cy2 = int(m['m01']/m['m00'])
+
+    cv2.circle(parte2,(cx1,cy1),10,(100,0,100),-1)
+    cv2.circle(parte3,(cx2,cy2),10,(100,0,100),-1)
+    
+    # position PID control
+    error = -(cx2 - width/2)/300
+
+    w = calculate_pid_angular(error)
+        
+    HAL.setW(w)
+    HAL.setV(4)
+
+    GUI.showImage(img)

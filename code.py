5c1c36214437b93b1ffa9c8ad51d570a2c7724fe
cescarcena2021 <102520602+cescarcena2021@users.noreply.github.com>
2023-11-06 18:37:58 +0100

    Update code.py

--- code.py
+++ code.py
@@ -19,80 +19,77 @@ def absolute2relative (x_abs, y_abs, robotx, roboty, robott):
 
     return x_rel, y_rel
 
-def parse_laser_data(laser_data):
+def parse_laser(laser_data):
     laser = []
     i = 0
   
     for i, dist in enumerate(laser_data.values):
-        if dist > 10:
-            dist = 10
         angle = math.radians(i-90)
         laser += [(dist, angle)]
-        i+=1
+        i += 1
     return laser
 
-def get_repulsive_force(parse_laser):
-    laser = parse_laser
-    
-    laser_vectorized = []
-    for dist, angle in laser:
-      
-        x = 1/dist * math.cos(angle) * -1
-        y = 1/dist * math.sin(angle) * -1
 
+def get_repulsive_force(parse_laser):
+
+    laser_array = []
+    for distance, angle in parse_laser:
+      
+        x = 1/distance * math.cos(angle) * -1
+        y = 1/distance * math.sin(angle) * -1
         v = (x,y)
-        laser_vectorized += [v]
-    laser_mean = np.mean(laser_vectorized, axis=0)
+
+        laser_array += [v]
+    laser_mean = np.mean(laser_array, axis=0)
     return laser_mean
 
 while True:
-    # Enter iterative code!
-    image=HAL.getImage()
+    # Get the image 
+    image = HAL.getImage()
+
+    # Get the laser and parse it 
     laser_data = HAL.getLaserData()
-    laser = parse_laser_data(laser_data)
-    #----------------------------------------------
-    # Obtener la posición absoluta del objetivo actual
-    currentTarget = GUI.map.getNextTarget()
-    GUI.map.targetx=currentTarget.getPose().x
-    GUI.map.targety=currentTarget.getPose().y
-    target_abs_x = currentTarget.getPose().x
-    target_abs_y = currentTarget.getPose().y
-    #----------------------------------------------
-    absolute_target = target_abs_x, target_abs_y
-    #----------------------------------------------
-    # Obtener la posición y orientación del robot
+    laser = parse_laser(laser_data)
+
+    # Get the robot position
     robot_x = HAL.getPose3d().x
     robot_y = HAL.getPose3d().y
     robot_yaw = HAL.getPose3d().yaw
-    #----------------------------------------------
-    target_rel_x,target_rel_y = absolute2relative(target_abs_x,target_abs_y,robot_x,robot_y,robot_yaw)
-    relative_target = target_rel_x, target_rel_y
+    
+    # Get the target absolute position
+    currentTarget = GUI.map.getNextTarget()
+    absolute_x = currentTarget.getPose().x
+    absolute_y = currentTarget.getPose().y
+    
+    # Transform the absolute target to the relative target
+    relative_x, relative_y = absolute2relative(absolute_x,absolute_y,robot_x,robot_y,robot_yaw)
+    relative_target = relative_x, relative_y
     
     # Car direction defined in a green vector
-    car_vect = [max(min(target_rel_x, 3.5), -3.5), max(min(target_rel_y, 3.2), -3.2)]
-    # obstacle direction defined in a red vector
-    obs_vect = [get_repulsive_force(laser)[0]*3, get_repulsive_force(laser)[1]*8]
-    # average direction defined in a black line
-    avg_vector = [(car_vect[0]+obs_vect[0]), (car_vect[1] + obs_vect[1]) *0.3]
+    atractive_vector = [max(min(relative_x, 3.5), -3.5), max(min(relative_y, 3.2), -3.2)]
+    # Obstacle direction defined in a red vector
+    repulsive_vector = [get_repulsive_force(laser)[0] * 3, get_repulsive_force(laser)[1] * 8]
+    # Average direction defined in a black line
+    total_vector = [(atractive_vector[0] + repulsive_vector[0]), (atractive_vector[1] + repulsive_vector[1]) * 0.3]
 
 
-    tan = math.tan(avg_vector[1]/avg_vector[0])
-
-    if (target_rel_x < 2 and target_rel_y < 2):
+    if (relative_x < 2 and relative_y < 2):
         currentTarget.setReached(True)
   
     
-    if(avg_vector[0] < 0):
+    if(total_vector[0] < 0):
         #turnig state
         HAL.setW(5)
         HAL.setV(1)
         
     else:
+        w_vel = math.tan(total_vector[1]/total_vector[0])
          
-        HAL.setW(tan * 2)
-        HAL.setV(avg_vector[0])
+        HAL.setW(w_vel * 2)
+        HAL.setV(total_vector[0])
         
+    # Show everything
     GUI.showImage(image)
     GUI.showLocalTarget(relative_target)
-    GUI.showForces(car_vect, obs_vect, avg_vector)
+    GUI.showForces(atractive_vector, repulsive_vector, total_vector)
     
